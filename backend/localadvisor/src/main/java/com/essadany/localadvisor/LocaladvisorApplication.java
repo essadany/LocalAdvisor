720e0eb80d657b517b9389ca179d452971e7771a
essadany <essadanyya@cy-tech.fr>
2025-03-11 16:40:24 +0100

    init backend project

---
+++ backend/localadvisor/src/main/java/com/essadany/localadvisor/LocaladvisorApplication.java
@@ -0,0 +1,13 @@
+package com.essadany.localadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaladvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaladvisorApplication.class, args);
+	}
+
+}
